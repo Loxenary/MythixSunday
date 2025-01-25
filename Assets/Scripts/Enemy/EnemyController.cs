6d943c9f778e69f629170390f603416ebbab02c7
Vylon-Kleio <rafaelmarchel.dw@gmail.com>
2025-01-25 18:24:35 +0700

    feat: implement knockback effect and grid snapping for enemy movement

--- Assets/Scripts/Enemy/EnemyController.cs
+++ Assets/Scripts/Enemy/EnemyController.cs
@@ -11,11 +11,10 @@ public class EnemyController : MonoBehaviour
     {
         currentHealth = enemyData.health;
         damage = enemyData.damage;
-        rb.GetComponent<Rigidbody2D>();
+        rb = GetComponent<Rigidbody2D>();
         if(rb == null)
         {
             rb = gameObject.AddComponent<Rigidbody2D>();
-            // rb.isKinematic = false;
         }
     }
 
@@ -36,8 +35,20 @@ public class EnemyController : MonoBehaviour
 
     private void ApplyKnockback(Vector2 direction, float force)
     {
-        rb.AddForce(direction * force, ForceMode2D.Impulse);
+        float gridSize = GridManager.Instance.GridSize;
+
+        Vector2 knockbackDirection = direction.normalized;
+        Vector2 newPosition = (Vector2)transform.position + knockbackDirection * gridSize;
+        newPosition = SnapToGrid(newPosition);
+        transform.position = newPosition;
         Debug.Log("Mundur");
+    }
+
+    protected Vector2 SnapToGrid(Vector2 position){
+        Grid grid = GridManager.Instance.GetComponent<Grid>();
+        Vector3Int cellPosition = grid.WorldToCell(position);
+        Vector3 snappedPosition = grid.CellToWorld(cellPosition) + grid.cellSize / 2f;
+        return new Vector2(snappedPosition.x, snappedPosition.y);
     }
 
     private void Die()
